b5a20ede40f6894be033fdae335b4a58358eaf08
Brandon <brandoncui@163.com>
2020-08-11 14:46:26 +0800

    Update MetricsEurekaApplication.java

--- metrics-eureka/src/main/java/com/brandon/metrics/eureka/MetricsEurekaApplication.java
+++ metrics-eureka/src/main/java/com/brandon/metrics/eureka/MetricsEurekaApplication.java
@@ -10,7 +10,7 @@ public class MetricsEurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MetricsEurekaApplication.class, args);
 		System.out.println("564646");
-		System.out.println("本地开发kkkkkkk");
+		System.out.println("本地开发jjjjj");
 		
 
 	}
